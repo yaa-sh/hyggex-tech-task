fc1731f91519be5dc1a55d721515b4e62c765199
Yash Khadikar <yashkhadikaralpha@gmail.com>
2024-05-08 20:28:18 +0530

    task completed

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  const sym = '>';
+  return (
+    <div className='overflow-x-hidden'>
+      <header className='flex gap-[40vw] p-4 min-w-[100vw] w-[100vw]'>
+        <div className=''>
+          <img src="/src/assets/logo.png" className='w-[200px] ml-10' alt="" />
+        </div>
+        <div className='flex '>
+              <span className='m-2 p-2 font-int text-[#3A3740] hover:cursor-pointer'>Home</span>
+              <span className='m-2 p-2 font-int text-[#3A3740] hover:cursor-pointer'>Flash Card</span>
+              <span className='m-2 p-2 font-int text-[#3A3740] hover:cursor-pointer'>Contact</span>
+              <span className='m-2 p-2 font-int text-[#3A3740] hover:cursor-pointer'>FAQs</span>
+        <span className='bg-gradient-to-r from-[#06286E] to-[#164EC0] p-1 pl-4 pr-4 m-2 rounded-2xl mb-3 text-[#fff] hover:cursor-pointer'>Login</span>
+        </div>
+
+
+      </header>
+      <div className='mt-10 ml-14 flex'>
+      <img src="/src/assets/vector.png" alt="" className='w-[25px]' />
+      <span className='hover:cursor-pointer text-xl ml-2 mr-2 font-bold text-[#06286E]'>{sym}</span>
+      <span className='hover:cursor-pointer text-[#696671]'>Flashcard</span>
+      <span className='hover:cursor-pointer text-xl ml-2 mr-2 font-bold text-[#06286E]'>{sym}</span>
+      <span className='hover:cursor-pointer text-[#696671]'>Mathematics</span>
+      <span className='hover:cursor-pointer text-xl ml-2 mr-2 font-bold text-[#06286E]'>{sym}</span>
+      <span className='hover:cursor-pointer text-[#06286E] font-bold'>Realtion and Function</span>
+
+      </div>
+      <main className='mt-10'>
+        <div className='flex flex-col'>
+          <div className='flex justify-center'>
+            <span className='hover:cursor-pointer font-bold m-2 p-2 text-[#06286E] border-b-2 border-[#06286E]'>Study</span>
+            <span className='hover:cursor-pointer text-[#3A3740]  m-2 p-2'>Quiz</span>
+            <span className='hover:cursor-pointer text-[#3A3740]  m-2 p-2'>Test</span>
+            <span className='hover:cursor-pointer text-[#3A3740]  m-2 p-2'>Game</span>
+            <span className='hover:cursor-pointer text-[#3A3740]  m-2 p-2'>Others</span>
+          </div>
+          <img src="/src/assets/main.png" className='w-[40%] lg:ml-[30%] md:ml-[27%]' alt="" />
+        </div>
+      </main>
+      <div className='flex justify-between m-4 lg:mr-40 lg:ml-40 md:mr-40 md:ml-40'>
+        <img src="/src/assets/right.png" className='w-[180px]' alt="" />
+        <img src="/src/assets/left.png" className='w-[200px] object-contain' alt="" />
+
+      </div>
+      <footer className='mt-20 lg:ml-20 md:ml-5'>
+        <span className='font-bold text-5xl bg-gradient-to-r from-[#06286E] to-[#164EC0] inline-block text-transparent bg-clip-text'>FAQ</span>
+        <div className='flex flex-col p-2 m-2'>
+          <div className='border-2 border-[#217EEC] rounded-xl m-2 p-4 lg:w-[60%] md:w-[60%] flex justify-between'>
+            <span>Can education flashcards be used for all age groups?</span>
+            <img src="/src/assets/downarrow.png" alt="" className='hover:cursor-pointer h-[10px]' />
+          </div>
+          <div className='border-2 border-[#217EEC] rounded-xl m-2 p-4 lg:w-[60%] md:w-[60%] flex justify-between'>
+            <span>How do education flashcards work?</span>
+            <img src="/src/assets/downarrow.png" alt="" className='hover:cursor-pointer h-[10px]' />
+          </div>
+          <div className='border-2 border-[#217EEC] rounded-xl m-2 p-4 lg:w-[60%] md:w-[60%] flex justify-between'>
+            <span>Can education flashcards be used for test preparation?</span>
+            <img src="/src/assets/downarrow.png" alt="" className='hover:cursor-pointer h-[10px]' />
+          </div>
+        </div>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
